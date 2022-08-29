55c4b17c21823a9b90434424a75dbedc7445735f
bia9400 <ferro.biagio@gmail.com>
2022-08-29 16:30:47 +0200

    modify

---
+++ resources/js/Frontend.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>{{message}}</h1>
+</template>
+
+
+
+<script>
+
+
+export default {
+    data(){
+        return{
+            message:"Ciao ciao con le mani"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
